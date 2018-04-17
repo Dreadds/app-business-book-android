3f39fd984cdc207d27b4818d16e295553fb54ee8
Dreadds <kevinatc10@gmail.com>
2018-04-16 19:35:55 -0500

    fragments

---
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/fragments/ReportsFragment.java
@@ -0,0 +1,30 @@
+package com.edu.upc.businessbook.viewcontrollers.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edu.upc.businessbook.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ReportsFragment extends Fragment {
+
+
+    public ReportsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reports, container, false);
+    }
+
+}
