11cab0a5d582388a16f1b20307931ba7cc4f2247
Dreadds <kevinatc10@gmail.com>
2018-04-16 19:28:45 -0500

    BottomNavigationView

---
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.edu.upc.businessbook.viewcontrollers.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.edu.upc.businessbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_reports:
+                    mTextMessage.setText(R.string.title_reports);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/OnBoardActivity.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/OnBoardActivity.java
@@ -1,4 +1,4 @@
-package com.edu.upc.businessbook;
+package com.edu.upc.businessbook.viewcontrollers.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.edu.upc.businessbook.R;
 
 public class OnBoardActivity extends AppCompatActivity {
 
