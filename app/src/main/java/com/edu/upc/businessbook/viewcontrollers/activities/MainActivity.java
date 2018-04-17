e067bec6d67a910b3dd55097cd5e88f23d43c14a
Dreadds <kevinatc10@gmail.com>
2018-04-16 19:48:26 -0500

    MainActivity

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/MainActivity.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/MainActivity.java
@@ -3,33 +3,27 @@ package com.edu.upc.businessbook.viewcontrollers.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.edu.upc.businessbook.R;
+import com.edu.upc.businessbook.viewcontrollers.fragments.HomeFragment;
+import com.edu.upc.businessbook.viewcontrollers.fragments.ProfileFragment;
+import com.edu.upc.businessbook.viewcontrollers.fragments.ReportsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_reports:
-                    mTextMessage.setText(R.string.title_reports);
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-            }
-            return false;
+
+            return navigateTo(item);
         }
     };
 
@@ -38,9 +32,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private Fragment getFragmentFor(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.navigation_home: return new HomeFragment();
+            case R.id.navigation_reports: return new ReportsFragment();
+            case R.id.navigation_profile: return new ProfileFragment();
+            default: return  new HomeFragment();
+        }
+    }
+
+    private boolean navigateTo(MenuItem item){
+        return getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, getFragmentFor(item))
+                .commit()>0;
     }
 
 }
