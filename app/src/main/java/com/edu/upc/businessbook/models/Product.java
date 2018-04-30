e3561f84468f1c919dfe3dd16b9c4adea84b115e
Dreadds <kevinatc10@gmail.com>
2018-04-30 17:42:59 -0500

    constructors class

--- app/src/main/java/com/edu/upc/businessbook/models/Product.java
+++ app/src/main/java/com/edu/upc/businessbook/models/Product.java
@@ -5,4 +5,14 @@ public class Product {
     private String name;
     private String unitPrice;
     private String state;
+
+    public Product(String prodcutId, String name, String unitPrice, String state) {
+        this.prodcutId = prodcutId;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.state = state;
+    }
+
+    public Product() {
+    }
 }
