7ddf04dfc82cacbbdb6e110a75e140a31c050671
Dreadds <kevinatc10@gmail.com>
2018-04-30 17:17:36 -0500

    getters, setters and patron changes

--- app/src/main/java/com/edu/upc/businessbook/models/Customer.java
+++ app/src/main/java/com/edu/upc/businessbook/models/Customer.java
@@ -27,4 +27,94 @@ public class Customer {
 
     public Customer() {
     }
+
+    public String getClienteId() {
+        return clienteId;
+    }
+
+    public Customer setClienteId(String clienteId) {
+        this.clienteId = clienteId;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Customer setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Customer setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Customer setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public Customer setDni(String dni) {
+        this.dni = dni;
+        return this;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public Customer setMail(String mail) {
+        this.mail = mail;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Customer setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public Customer setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+        return this;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public Customer setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+        return this;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Customer setState(String state) {
+        this.state = state;
+        return this;
+    }
 }
