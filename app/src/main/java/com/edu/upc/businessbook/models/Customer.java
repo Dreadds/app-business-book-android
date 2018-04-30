7d64aeab79460226848f9a594be924b7c88841c1
Dreadds <kevinatc10@gmail.com>
2018-04-30 17:15:47 -0500

    constructors class

--- app/src/main/java/com/edu/upc/businessbook/models/Customer.java
+++ app/src/main/java/com/edu/upc/businessbook/models/Customer.java
@@ -11,4 +11,20 @@ public class Customer {
     private String creationDate;
     private String updateDate;
     private String state;
+
+    public Customer(String clienteId, String name, String lastName, String fullName, String dni, String mail, String phone, String creationDate, String updateDate, String state) {
+        this.clienteId = clienteId;
+        this.name = name;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.dni = dni;
+        this.mail = mail;
+        this.phone = phone;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+        this.state = state;
+    }
+
+    public Customer() {
+    }
 }
