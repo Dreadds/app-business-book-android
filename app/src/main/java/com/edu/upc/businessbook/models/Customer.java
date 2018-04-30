342d9627bdc9445741148c33214b1f09bee8bb75
Dreadds <kevinatc10@gmail.com>
2018-04-30 17:12:23 -0500

    attributes customer

--- app/src/main/java/com/edu/upc/businessbook/models/Customer.java
+++ app/src/main/java/com/edu/upc/businessbook/models/Customer.java
@@ -1,4 +1,14 @@
 package com.edu.upc.businessbook.models;
 
 public class Customer {
+    private String clienteId;
+    private String name;
+    private String lastName;
+    private String fullName;
+    private String dni;
+    private String mail;
+    private String phone;
+    private String creationDate;
+    private String updateDate;
+    private String state;
 }
