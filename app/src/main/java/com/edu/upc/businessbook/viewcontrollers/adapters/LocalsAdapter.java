cb68fd2962f8cef236c4878f5325eab11c82b1b5
Dreadds <kevinatc10@gmail.com>
2018-05-01 02:56:42 -0500

    implements local adapter

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/adapters/LocalsAdapter.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/adapters/LocalsAdapter.java
@@ -1,5 +1,31 @@
 package com.edu.upc.businessbook.viewcontrollers.adapters;
 
-public class LocalsAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class LocalsAdapter extends RecyclerView.Adapter<LocalsAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public LocalsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LocalsAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
