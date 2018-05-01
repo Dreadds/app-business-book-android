cb4648a862089279bfd86bb593bd180a760de085
Dreadds <kevinatc10@gmail.com>
2018-05-01 05:03:22 -0500

    LocalActivity

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/adapters/LocalsAdapter.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/adapters/LocalsAdapter.java
@@ -1,5 +1,7 @@
 package com.edu.upc.businessbook.viewcontrollers.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 
 import com.edu.upc.businessbook.R;
 import com.edu.upc.businessbook.models.Local;
+import com.edu.upc.businessbook.viewcontrollers.activities.LocalActivity;
 
 import java.util.List;
 
@@ -64,9 +67,16 @@ public class LocalsAdapter extends RecyclerView.Adapter<LocalsAdapter.ViewHolder
             exploreTextView = (TextView) view.findViewById(R.id.exploreTextView);
             localLayout = (CardView) view.findViewById(R.id.layout_local);
         }
-        private void updateView(Local local){
+        private void updateView(final Local local){
             titleTextView.setText(local.getName());
             nameTextView.setText(local.getAddress());
+            localLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    context.startActivity(new Intent(context, LocalActivity.class));
+                }
+            });
         }
     }
 }
