006965d9342342d27a8431000bb48d61ff8bf785
Dreadds <kevinatc10@gmail.com>
2018-05-01 03:09:14 -0500

    localsAdapterimpl

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/adapters/LocalsAdapter.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/adapters/LocalsAdapter.java
@@ -1,9 +1,14 @@
 package com.edu.upc.businessbook.viewcontrollers.adapters;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.edu.upc.businessbook.R;
 
 public class LocalsAdapter extends RecyclerView.Adapter<LocalsAdapter.ViewHolder> {
 
@@ -24,8 +29,19 @@ public class LocalsAdapter extends RecyclerView.Adapter<LocalsAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
+        private ImageView localImageView;
+        private TextView titleTextView;
+        private TextView nameTextView;
+        private TextView exploreTextView;
+        private CardView localLayout;
+        public ViewHolder(View view) {
+            super(view);
+            localImageView = (ImageView) view.findViewById(R.id.localImageView);
+            titleTextView = (TextView) view.findViewById(R.id.titleTextView);
+            nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+            exploreTextView = (TextView) view.findViewById(R.id.exploreTextView);
+            localLayout = (CardView) view.findViewById(R.id.layout_local);
         }
+
     }
 }
