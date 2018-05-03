15c235376a9ac94f19eda6f87c9b715efb8e2f32
Dreadds <kevinatc10@gmail.com>
2018-05-03 10:23:25 -0500

    dialog finish

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/dialogs/DialogPersonalized.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/dialogs/DialogPersonalized.java
@@ -2,21 +2,51 @@ package com.edu.upc.businessbook.viewcontrollers.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 import com.edu.upc.businessbook.R;
+import com.edu.upc.businessbook.models.Local;
+import com.edu.upc.businessbook.models.LocalsRepository;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DialogPersonalized extends DialogFragment {
+
+    TextInputEditText editText;
+    LocalsRepository localsRepository;
+
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setView(getActivity().getLayoutInflater().inflate(R.layout.dialog_local,null));
+        View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_local,null);
+        editText = (TextInputEditText) view.findViewById(R.id.editLocalName);
+        builder.setView(view)
+                .setTitle("Create Local")
+                .setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        localsRepository.getLocals().add(new Local(editText.getText().toString(),"1",R.drawable.ic_business_black_24dp));
+                        //locals.add(new Local(editText.getText().toString(),"1",R.drawable.ic_business_black_24dp));
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
         return builder.create();
     }
 }
