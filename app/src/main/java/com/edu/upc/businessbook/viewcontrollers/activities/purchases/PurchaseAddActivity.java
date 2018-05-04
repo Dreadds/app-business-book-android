4f3401429f710b005b71b53977b7fca7cebe5c24
Josemaria <josemaria@MacBook-Pro-de-Josemaria.local>
2018-05-04 09:07:08 -0500

    tercer commit

---
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/purchases/PurchaseAddActivity.java
@@ -0,0 +1,13 @@
+package com.edu.upc.businessbook.viewcontrollers.activities.purchases;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PurchaseAddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_add);
+    }
+}
---
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/sales/SaleAddActivity.java
@@ -0,0 +1,13 @@
+package com.edu.upc.businessbook.viewcontrollers.activities.sales;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SaleAddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_add);
+    }
+}
