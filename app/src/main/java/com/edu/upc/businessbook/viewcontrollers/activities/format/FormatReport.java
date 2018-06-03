bfdf2513112cc063236f8a843269f141913ea83f
ANA <pau.santillan17@gmail.com>
2018-06-02 21:08:59 -0500

    commit format report

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/format/FormatReport.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/format/FormatReport.java
@@ -1,4 +1,49 @@
 package com.edu.upc.businessbook.viewcontrollers.activities.format;
 
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DecimalFormat;
+
 public class FormatReport {
+
+    public class MyXAxisValueFormatter implements IAxisValueFormatter {
+        private String[] xValues;
+        public MyXAxisValueFormatter(String[] values){
+            this.xValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return xValues[(int)value];
+        }
+    }
+
+    public class MyYAxisValuesFormatter implements IAxisValueFormatter{
+        private DecimalFormat mFormat;
+
+        public  MyYAxisValuesFormatter(){
+            mFormat = new DecimalFormat("###,###,##0.0");
+        }
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return "S/." + mFormat.format(value);
+        }
+    }
+
+    public class MyValueFormatter implements IValueFormatter {
+        private DecimalFormat mFormat;
+
+        public  MyValueFormatter(){
+            mFormat = new DecimalFormat("###,###,##0.0");
+        }
+
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            return "S/." + mFormat.format(value);
+        }
+    }
 }
