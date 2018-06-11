34a18771bb859f53f90096f99ee3e603b9913b2d
Chema19 <chemalonso96@gmail.com>
2018-06-11 15:49:23 -0500

    commit comida

---
+++ app/src/main/java/com/edu/upc/businessbook/models/SaleDetail.java
@@ -0,0 +1,65 @@
+package com.edu.upc.businessbook.models;
+
+public class SaleDetail {
+    public int SaleDetailId;
+    public int ProductId;
+    public int Quantity;
+    public float UnitPrice;
+    public float PriceSubTotal;
+
+    public SaleDetail() {
+    }
+
+    public SaleDetail(int saleDetailId, int productId, int quantity, float unitPrice, float priceSubTotal) {
+        SaleDetailId = saleDetailId;
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+        PriceSubTotal = priceSubTotal;
+    }
+
+    public int getSaleDetailId() {
+        return SaleDetailId;
+    }
+
+    public SaleDetail setSaleDetailId(int saleDetailId) {
+        SaleDetailId = saleDetailId;
+        return this;
+    }
+
+    public int getProductId() {
+        return ProductId;
+    }
+
+    public SaleDetail setProductId(int productId) {
+        ProductId = productId;
+        return this;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public SaleDetail setQuantity(int quantity) {
+        Quantity = quantity;
+        return this;
+    }
+
+    public float getUnitPrice() {
+        return UnitPrice;
+    }
+
+    public SaleDetail setUnitPrice(float unitPrice) {
+        UnitPrice = unitPrice;
+        return this;
+    }
+
+    public float getPriceSubTotal() {
+        return PriceSubTotal;
+    }
+
+    public SaleDetail setPriceSubTotal(float priceSubTotal) {
+        PriceSubTotal = priceSubTotal;
+        return this;
+    }
+}
