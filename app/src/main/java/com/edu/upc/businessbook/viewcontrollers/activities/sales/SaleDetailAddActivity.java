49de80c791e5c35886aab787a892441d9f28a829
Chema19 <chemalonso96@gmail.com>
2018-06-11 14:01:06 -0500

    commit :V

---
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/activities/sales/SaleDetailAddActivity.java
@@ -0,0 +1,13 @@
+package com.edu.upc.businessbook.viewcontrollers.activities.sales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaleDetailAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_detail_add);
+    }
+}
