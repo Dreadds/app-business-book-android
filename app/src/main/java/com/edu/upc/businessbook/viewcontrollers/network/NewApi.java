c1a7f0b3aa8ef05a55b8cee880c3d0b0f584adb6
KatherinChuco <katherin.chuco@gmail.com>
2018-06-18 21:43:20 -0500

    conexion al API con tokens

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/network/NewApi.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/network/NewApi.java
@@ -1,8 +1,10 @@
 package com.edu.upc.businessbook.viewcontrollers.network;
 
 public class NewApi {
-    private static String BASE_URL = "http://chemita96-001-site1.dtempurl.com/businessbookapi";
+    private static String BASE_URL = "http://chemita96-001-site1.dtempurl.com/businessbookapi/v1";
 
     public static String getSaleUrl() { return BASE_URL + "/sales"; }
     public static String getPurchaselUrl() { return BASE_URL + "/purchases"; }
+    public static String postUserRegisterUrl(){ return BASE_URL + "/registeremployee"; }
+    public static String postUserLoginUrl(){ return BASE_URL + "/login"; }
 }
