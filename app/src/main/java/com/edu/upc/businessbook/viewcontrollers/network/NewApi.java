9b4b23d6242e557ae4833e6cbf2b937f7c9cdf0f
Chema19 <chemalonso96@gmail.com>
2018-06-23 16:04:07 -0500

    chema

--- app/src/main/java/com/edu/upc/businessbook/viewcontrollers/network/NewApi.java
+++ app/src/main/java/com/edu/upc/businessbook/viewcontrollers/network/NewApi.java
@@ -3,16 +3,16 @@ package com.edu.upc.businessbook.viewcontrollers.network;
 import android.view.View;
 
 public class NewApi {
-    private static String BASE_URL = "http://chemita96-001-site1.dtempurl.com/";
+    private static String BASE_URL = "http://chemita96-001-site1.dtempurl.com/businessbookapi/v1";
     //GET
-    public static String getListSaleUrl(int companyId) { return BASE_URL + "businessbookapi/v1/companies/"  + String.valueOf(companyId) + "/sales"; }
-    public static String getListClient(int companyId){return BASE_URL + "businessbookapi/v1/companies/" + String.valueOf(companyId) + "/clients"; }
-    public static String getListLocal(int companyId){return BASE_URL + "businessbookapi/v1/companies/" + String.valueOf(companyId) + "/locals"; }
-    public static String getListProduct(int companyId){return BASE_URL + "businessbookapi/v1/companies/" + String.valueOf(companyId) + "/products"; }
+    public static String getListSaleUrl(int companyId) { return BASE_URL + "/companies/"  + String.valueOf(companyId) + "/sales"; }
+    public static String getListClient(int companyId){return BASE_URL + "/companies/" + String.valueOf(companyId) + "/clients"; }
+    public static String getListLocal(int companyId){return BASE_URL + "/companies/" + String.valueOf(companyId) + "/locals"; }
+    public static String getListProduct(int companyId){return BASE_URL + "/companies/" + String.valueOf(companyId) + "/products"; }
 
 
     //POST
-    public static String postSale(){ return BASE_URL + "businessbookapi/v1/sales"; }
-    public static String postSaleDetail(int saleId){ return BASE_URL + "businessbookapi/v1/sales/" + String.valueOf(saleId) +"/items"; }
+    public static String postSale(){ return BASE_URL + "/sales"; }
+    public static String postSaleDetail(int saleId){ return BASE_URL + "/sales/" + String.valueOf(saleId) +"/items"; }
     public static String getPurchaselUrl() { return BASE_URL + "/purchases"; }
 }
